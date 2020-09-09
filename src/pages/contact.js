59f39e1460f88eab3809601c68adacc055362528
Tomoaki Morioka <61305041+Tom-M-Git@users.noreply.github.com>
2020-09-09 15:40:51 +0900

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,7 @@ const ContactPage = () => {
       <Layout>
         <Head title="Contact" />
         <h1>Contact</h1>
-        <p>The best way to reach me is via <a href="https://www.linkedin.com/in/tomoaki-m-4078a31a1" target="_blank" rel="noreferrer">Tomoaki Morioka</a> on LinkedIn.</p>
+        <p>The best way to reach me is via <a href="#" target="_blank" rel="noreferrer">(goes nowhere)</a></p>
       </Layout>
   );
 };
